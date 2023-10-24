b6186553f4af87eb0d2044f4a014ce263c55d26c
axpico <ale@picone.it>
2023-10-24 11:48:22 +0200

    sistemo codice

--- codice/MetodiEs.java
+++ codice/MetodiEs.java
@@ -5,7 +5,7 @@
     crea un metodo che dica se 3 numeri possono creare un triangolo
     crea un metodo che dica la lunghezza del numero
     crea un metodo che trovi tra tre numeri il maggiore
-    crea un metodo che disegni un rompo
+    crea un metodo che disegni un rombo
 */
 
 public class MetodiEs {
@@ -13,41 +13,88 @@ public class MetodiEs {
 
     }
 
-
     public static char MinAMaius (char a){
-        return a;
-    }
-    
-
-    public static int NumPrimo (int a){
-        return a;
+        return (char) (a - 32);
     }
 
+    public static boolean NumPrimo (int a){
+        if (a <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static double AreaCerchio (int a){
         double Area = 0;
         return Area;
     }
 
-
     public static boolean TreNumTriangolo (int a, int b, int c){
-        boolean vero = true;
-        return vero;
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
+        return false;
     }
 
-
     public static int LunghezzaNum (int a){
+        if(a == 0) {
+            return 1;
+        }
         int lunghezza = 0;
+        while (a != 0) {
+            a /= 10;
+            lunghezza++;
+        }
         return lunghezza;
     }
 
-
     public static int ValMagg (int a, int b, int c){
-        int valoreMagggiore = 0;
-        return valoreMagggiore;
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
     }
 
-
-    public static void CirconferenzaCognometrica (int a){
+    public static void DisegnoRombo (int n){
+        if (n % 2 == 0) {
+            n++; // se n è pari si incrementa di 1
+        }
+        int spazi = n / 2;
+        int asterischi = 1;
+        // triangolo sopra
+        for (int i = 0; i < n / 2 + 1; i++) {
+            for (int j = 0; j < spazi; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < asterischi; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            spazi--;
+            asterischi += 2;
+        }
+        // triangolo sotto
+        spazi = 1;
+        asterischi = n - 2;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < spazi; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < asterischi; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            spazi++;
+            asterischi -= 2;
+        }
     }
 }
--- codice/Test.java
+++ codice/Test.java
@@ -1,6 +1,3 @@
 public interface Test {
-    public static void main(String[]args){
-        NumeriPerfetti.main(args);
-
-    }
+    
 }
