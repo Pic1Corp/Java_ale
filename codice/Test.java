5fbbc61c6000521526f607f07a00cabe598f5d0e
axpico <ale@picone.it>
2023-10-26 09:00:23 +0200

    array es

--- codice/Test.java
+++ codice/Test.java
@@ -1,3 +1,5 @@
 public interface Test {
-    
+    public static void main(String[] args){
+        
+    }
 }
---
+++ codice/array.java
@@ -0,0 +1,15 @@
+/*
+ * costruisci un array e inizzializalo con tutti i numeri pardi di dimensione 100 e stampa a schermo
+ * inizzializza un array di 100 posizioni con le lettere dell'alfabeto minuscole 
+ * inizzializza un array di 100 posizioni con le posioni pari i numeri pari e nelle posioni dispari con multipli di 7
+ * inizzializza un array di 100 posizioni con i divisori di un numero preso in input
+ * inizzializza un array di 100 posizioni con numeri presi da input e la sequenza è terminata con 0 
+ * inizzializza un array di 100 posizioni con i numeri della sequenza di fibonacci
+ * inizzializza un array di 100 posizioni con i fattoriali con un numero preso in input 
+ */
+
+public class array {
+    public static void main (String[]arg){
+        
+    }
+}
