26b3233667b0cc07b94ff18849d6d19d15a0461b
axpico <ale@picone.it>
2023-11-04 13:03:27 +0100

    fix it

--- codice/TestMatematicaEs1.java
+++ codice/TestMatematicaEs1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class TestMatematicaEs {
+public class TestMatematicaEs1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int inputU = 0;
