52c32c9abfd1e6a6ba6bc3511d77c4628dd9907f
axpico <119961943+axpico@users.noreply.github.com>
2023-12-04 15:23:26 +0100

    Update AlgoritmiImportanti.java

--- codice/AlgoritmiImportanti.java
+++ codice/AlgoritmiImportanti.java
@@ -193,4 +193,13 @@ public class AlgoritmiImportanti {
         }
         return array;
     }
+
+    public static void giraArray(int[] array){
+        int temp;
+        for (int i = 0; i < array.length/2; i++) {
+            temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+    }
 }
